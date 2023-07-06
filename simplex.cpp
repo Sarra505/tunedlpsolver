86eff0f8b4a282755db6e6c6858f2eb85e46be2b
sarrabenmed <sarrabenmeds@gmail.com>
2023-07-06 19:28:20 +0200

    add new chapter

---
+++ simplex.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+class Simplex{
+    private: 
+    int rows, cols;
+    //stores coefficients of all the variables
+    std::vector <std::vector<float>> A;
+    //stores constants of conctraints 
+    std::vector
+
+
+}
