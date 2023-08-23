1a0eef86b123d923511b8842605d05292261db36
sarrabenmohamed <sarra.ben-mohamed@tum.de>
2023-08-23 23:20:13 +0200

    solve issues

--- revisedSimplex.cpp
+++ revisedSimplex.cpp
@@ -434,7 +434,11 @@ std::tuple<std::vector<Entry>, unsigned, unsigned> parseLPProblem(const std::str
       for (int j = 0; j < numEntries; j++)
       {
          int columnNumber;
+<<<<<<< HEAD
          double coefficient;
+=======
+         float coefficient;      
+>>>>>>> 1b6c65c (solve issues)
          iss >> columnNumber >> coefficient;
          entry.coef = coefficient;
          entry.rule = i;
